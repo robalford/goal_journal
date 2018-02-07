0f9146be9b2e9d235f4c38a8aa556a1cfbea8b08
Robert Alford <robertsalford@gmail.com>
2018-02-07 13:53:03 -0800

    Goals app initial models and migrations

---
+++ goal_journal/goals/choices.py
@@ -0,0 +1,10 @@
+FAIL = 0
+WIP = 5
+SUCCESS = 10
+
+ACTION_STATUS = (
+    (None, 'Rate your progress'),
+    (FAIL, 'Fail'),
+    (WIP, 'WIP'),
+    (SUCCESS, 'Success!'),
+)
--- goal_journal/goals/models.py
+++ goal_journal/goals/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+from goal_journal.users.models import User
+from . import choices
+
+
+class Category(models.Model):
+    user = models.ForeignKey(User)
+    category = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.category
+
+
+class Goal(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    goal = models.TextField()
+    date_created = models.DateTimeField(auto_now=True)
+    categories = models.ManyToManyField(Category, related_name='goals')
+    actions = models.ManyToManyField('Action', related_name='goals')
+    target_date = models.DateField(null=True, blank=True)
+    goal_achieved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.goal
+
+
+class Action(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    action = models.TextField(blank=True)
+    priority = models.SmallIntegerField(blank=True, null=True)
+    action_completed = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['priority', ]
+
+    def __str__(self):
+        return self.action
+
+
+class ActionLog(models.Model):
+    action = models.ForeignKey(Action, on_delete=models.CASCADE, null=True)
+    action_status = models.IntegerField(choices=choices.ACTION_STATUS, blank=True, null=True)
+    status_logged = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-status_logged', ]
+
+    def __str__(self):
+        return '{}: {} on {}'.format(self.action_status, self.action, self.status_logged)
+
+    def get_action_status_class(self):
+        if self.action_status == 0:
+            bootstrap_class = 'danger'
+        elif self.action_status == 5:
+            bootstrap_class = 'warning'
+        else:
+            bootstrap_class = 'success'
+        return bootstrap_class
