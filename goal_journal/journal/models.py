80f3a6586a0edcd9ca7a6c46bf3472e2add5bdfb
Robert Alford <robertsalford@gmail.com>
2018-02-07 13:56:20 -0800

    Journal app initial models and migraitions

--- goal_journal/journal/models.py
+++ goal_journal/journal/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from goal_journal.goals.models import Goal, Action
+from goal_journal.users.models import User
+
+
+class Entry(models.Model):
+    goal = models.ForeignKey(Goal, on_delete=models.CASCADE)
+    progress = models.TextField()
+    date_of_entry = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'Entries'
+        ordering = ['-date_of_entry', ]
+
+    def __str__(self):
+        return '{} {}'.format(self.goal, self.date_of_entry)
